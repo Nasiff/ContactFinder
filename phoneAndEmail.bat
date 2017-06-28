d8fd7eac88b3b813f2ccbbcc90edd5e659909cf6
Md Nasiful Haque <nasif_fcb_haque@yahoo.com>
2017-06-28 21:51:45 +0300

    Rename phoneAndEmail.bat to src.bat

--- phoneAndEmail.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\Nasif619\AppData\Local\Programs\Python\Python36\python.exe C:\Users\Nasif619\AppData\Local\Programs\Python\Python36\MyPythonScripts\phoneAndEmail.py
---
+++ src.bat
@@ -0,0 +1,1 @@
+C:\Users\Nasif619\AppData\Local\Programs\Python\Python36\python.exe C:\Users\Nasif619\AppData\Local\Programs\Python\Python36\MyPythonScripts\phoneAndEmail.py
